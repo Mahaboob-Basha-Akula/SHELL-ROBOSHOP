aaf328a085ea130ffbe580e464a7f48e3a972ee0
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-06 23:04:11 +0530

    another scriptt

---
+++ 02-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-051b1f8e584ea35b8"
+
+
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-051b1f8e584ea35b8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=TestInstance}]" --query 'Instances[0].InstanceId' --output text)
+
+done
