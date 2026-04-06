1e48c8887dd4c61e2403fe0ed097ef0bd2a2651c
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-06 22:51:42 +0530

    Creating ec2 by using shell-script

--- 01-instance.sh
+++ 01-instance.sh
@@ -8,7 +8,7 @@ for instance in $@
 do
     # Instance_Id = $(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-    INSTANCE_ID = $(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance = frontend ]; then 
         IpAddress=$(aws ec2 describe instances --instance_id $Instance_Id --query "Reservations[0].Instances[0].PublicIpAddress" --outpu text)
