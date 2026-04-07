6194b9c60d5ef4c44ec68af88acc9d94ac402ed7
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-07 23:31:58 +0530

    practice script

---
+++ 03-install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0cb74036ffa409ec3"
+ZONE_ID="Z079516023DG9ZEN0RSC9"
+DOMAIN_NAME="learnwithmahaboob.cyou"
+
+for instance in $@
+do
+INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    if [ $instance = frontend ]; then
+        IP=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    fi
+    echo "$instance:$IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '{
+        "Comment": "Updating A record for $instance",
+        "Changes": [{
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "$RECORD_NAME",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [{ "$Ip" }]
+            }
+        }]
+    }'
+done
+
+
+
+
