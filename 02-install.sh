1488399398fef9e163143eaf47826c85bb887212
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-07 21:46:37 +0530

    mistake updated

--- 02-install.sh
+++ 02-install.sh
@@ -6,7 +6,7 @@ SG_ID="sg-051b1f8e584ea35b8"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-051b1f8e584ea35b8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
         Ip=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
