58fa2e5d7d19356f0d2e10ba5658b2774bed09ff
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-07 21:41:31 +0530

    mistake updated

--- 02-install.sh
+++ 02-install.sh
@@ -9,9 +9,9 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-051b1f8e584ea35b8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "fronend" ]; then
-        Ip=$(aws ec2 describe-instances --instance_id $Instance_Id --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        Ip=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
-        Ip=$(aws ec2 describe-instances --instance_id $Instance_Id --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        Ip=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
 
     echo "$instance:$Ip"
