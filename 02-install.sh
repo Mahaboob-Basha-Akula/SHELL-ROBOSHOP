bb5bd567882933494e460404b9f1e85c6837fefd
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-07 21:38:12 +0530

    another scriptt

--- 02-install.sh
+++ 02-install.sh
@@ -6,6 +6,16 @@ SG_ID="sg-051b1f8e584ea35b8"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-051b1f8e584ea35b8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=TestInstance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-051b1f8e584ea35b8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    if [ $instance != "fronend" ]; then
+        Ip=$(aws ec2 describe-instances --instance_id $Instance_Id --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        Ip=$(aws ec2 describe-instances --instance_id $Instance_Id --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
+
+    echo "$instance:$Ip"
+
+    
 
 done
