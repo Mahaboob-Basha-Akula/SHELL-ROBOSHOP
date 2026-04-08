aeb54287c0efb9a1cd5fab27ff45065a53f4009b
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-08 19:52:53 +0530

    Redirections added

--- 04-mongodb.sh
+++ 04-mongodb.sh
@@ -8,10 +8,16 @@ P="\e[35m"
 
 USER_ID=$(id -u)
 
+LOG_FOLDER="/var/log/shell-roboshop/"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
 if [ $USER_ID -ne 0 ]; then
     echo -e "$R ERROR:: Need root privilages $N "
     exit 1
 fi
+
+mkdir -p $LOG_FOLDER
 
 VALIDATE(){
     if [ $1 -ne  0 ]; then
@@ -22,30 +28,30 @@ VALIDATE(){
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 VALIDATE $? "Adding mongo repo"
 
 
 
 
-dnf list installed mongodb-org -y
+dnf list installed mongodb-org -y &>>$LOG_FILE
 if [ $? -ne 0 ]; then 
-    dnf install mongodb-org -y
+    dnf install mongodb-org -y &>>$LOG_FILE
     VALIDATE $? "mongodb"
 else 
     echo -e "$G $2 is already exists... $N $Y SKIPPING $N "
 fi
 
 
-systemctl enable mongod 
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enabling mongodb"
 
-systemctl start mongod
+systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Starting mongodb"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections to MongoDB"
 
-systemctl restart mongod
+systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarted MongoDB"
 
