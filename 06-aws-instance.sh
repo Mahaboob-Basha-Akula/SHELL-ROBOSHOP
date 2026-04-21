43f727bdfa309657fb6e6d6d653f099c5b2d2426
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-21 11:09:45 +0530

    instance  added

--- 06-aws-instance.sh
+++ 06-aws-instance.sh
@@ -6,18 +6,18 @@ SG_ID="sg-0cb74036ffa409ec3"
 ZONE_ID="Z079516023DG9ZEN0RSC9"
 DOMAIN_NAME="learnwithmahaboob.cyou"
 
-for instances in $@
+for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro        --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instances}]' --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro        --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
 
     if [ $INSTANCE_ID = frontend ]; then
         INSTANCE_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
         RECORD_NAME=$DOMAIN_NAME
     else
         INSTANCE_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME=$instances.$DOMAIN_NAME
+        RECORD_NAME=$instance.$DOMAIN_NAME
     fi
-    echo "$instances: $INSTANCE_IP"
+    echo "$instance: $INSTANCE_IP"
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
