3d53f90168c8edc0fc9350edf17d7434ddf22efd
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-22 23:43:52 +0530

    redis added

--- 07-redis.sh
+++ 07-redis.sh
@@ -23,9 +23,9 @@ mkdir -p $LOG_FOLDER
 
 VALIDATE(){
     if [ $1 -ne  0 ]; then
-        echo -e " $G $2 success $N"
+        echo -e " $G $2 FAILED $N"
     else
-        echo -e "$R $2 failed $N"
+        echo -e "$R $2 SUCCESS $N"
     fi
 }
 
