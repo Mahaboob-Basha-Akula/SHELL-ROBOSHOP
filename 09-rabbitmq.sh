e7421c5280de7d62dbc39e7c76038bcdb8c2b75a
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-23 23:11:47 +0530

    rabbitmq added

---
+++ 09-rabbitmq.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+P="\e[35m"
+
+
+
+LOG_FOLDER="/var/log/rabbitmq-shell"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+    echo -e "$R ERROR:: Need root privilages $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then 
+        echo -e " $R Package install is FAILED $N"
+    else 
+        echo -e "$G Package install is SUCCESS $N"
+    fi
+}
+
+
+dnf install rabbitmq-server -y
+VALIDATE $? "Rabbitmq installation"
+
+systemctl enable rabbitmq-server
+VALIDATE $? "Enabling rabbitmq"
+
+systemctl start rabbitmq-server
+VALIDATE $? "Rabbitmq Started"
+
+rabbitmqctl add_user roboshop roboshop123
+VALIDATE $? "Adding new user"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "Settimmg permissions to roboshop user"
+
